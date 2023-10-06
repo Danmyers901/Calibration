7d128abd41730cd683d5c17e39c14f232f8d07cd
Danmyers901 <71978159+Danmyers901@users.noreply.github.com>
2023-10-06 08:49:08 -0400

    Add files via upload

--- Pixel_class_probabilities/R/Transfer/Transfer evals built figure.R
+++ Pixel_class_probabilities/R/Transfer/Transfer evals built figure.R
@@ -1,1 +1,299 @@
+# BUCK evals figure (built)
+# Dan Myers, 5/14/2023
 
+
+# Set working directory
+setwd("R:/NPS_NCRN_VitalSigns/Analyses/Projects/AGU23/Scripts/R/Transfer/")
+
+# Load packages
+library(raster)
+library(terra)
+library(unikn) # for text mark
+library(FedData) # NLCD palette
+library(rasterVis) # for levelplot
+library(sf)
+library(tmap)
+library(tmaptools)
+
+# Start plot
+windows(6.5,6.5)
+par(mar=c(1,1,1,1))
+nf <- layout(matrix(c(1,2, # top
+                      3,4, # middle
+                      5,5), # bottom
+                    nrow=3, ncol=2,byrow=TRUE))
+
+
+### a) NLCD ###########################################################
+# Load data
+nlcd <- raster("align2_nlcd.tif")
+shed_orig <- st_read("MONO_BUCK_shed.shp")
+shed <- st_transform(shed_orig, crs=st_crs(nlcd))
+
+nlcd_open <- reclassify(nlcd, matrix(c(0,0,0,
+                                       0,20,0,
+                                       20,21,1,
+                                       21,100,0),ncol=3, byrow=T))
+
+nlcd_low <- reclassify(nlcd, matrix(c(0,0,0,
+                                      0,21,0,
+                                      21,22,2,
+                                      22,100,0),ncol=3, byrow=T))
+
+nlcd_med <- reclassify(nlcd, matrix(c(0,0,0,
+                                      0,22,0,
+                                      22,23,3,
+                                      23,100,0),ncol=3, byrow=T))
+
+nlcd_high <- reclassify(nlcd, matrix(c(0,0,0,
+                                       0,23,0,
+                                       23,24,4,
+                                       24,100,0),ncol=3, byrow=T))
+
+nlcd_urb <- nlcd_open + nlcd_low + nlcd_med + nlcd_high
+
+# Plot it
+plot(nlcd_urb, col=c("#FFFFFF","#E8D1D1","#E29E8C","#ff0000","#B50000"),legend=FALSE,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+legend("topright",legend=c("Open","Low","Medium","High"), fill=c("#E8D1D1","#E29E8C","#ff0000","#B50000"),bg="white")
+title("a)",adj=0.02, line=-1.2, cex.main=1.5)
+title("NLCD19 urban classes",adj=0.02, line=-13.9, cex.main=1)
+
+
+
+
+### b) DW original ###########################################################
+# Load data
+dw_comp_uncrop <- raster("align2_comp.tif")
+
+# Crop to watershed
+dw_comp <- mask(dw_comp_uncrop,shed)
+
+# Extract urban from DW composite
+dw_comp_urb <- reclassify(dw_comp, matrix(c(0,0,NA,
+                                            0,5,NA,
+                                            5,6,1,
+                                            6,10,NA),ncol=3, byrow=T))
+
+# Plot it
+plot(dw_comp_urb, col='#C4281B',legend=FALSE,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+legend("topright",legend="Built", fill=c('#C4281B'),bg="white")
+title("b)",adj=0.02, line=-1.2, cex.main=1.5)
+title("DW22 built class",adj=0.02, line=-13.9, cex.main=1)
+
+
+
+
+### c) DW pixel probs##########################################################
+# Load data
+dw <- raster("align2_built.tif")
+
+# Mask tree land in DW probs
+dw_urb <- dw * dw_comp_urb
+
+# Set colors and plot it
+plot(dw_urb,legend=T,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+title("c)",adj=0.02, line=-1.2, cex.main=1.5)
+title("DW22 built probability",adj=0.02, line=-13.9, cex.main=1)
+
+
+
+### d) DW sub-classified#######################################################
+##### Set Bins #####
+bin_open_low <-53 # highest value in lower bin, 53 is good
+bin_low_med <-66 # 66 is good
+bin_med_high <-70 # 70 is good
+
+# Extract forest types
+dw_open <- reclassify(dw_urb, matrix(c(0,0,0,
+                                       0,0,0,
+                                       0,bin_open_low,1,
+                                       bin_open_low,100,0),ncol=3, byrow=T))
+
+dw_low <- reclassify(dw_urb, matrix(c(0,0,0,
+                                      0,bin_open_low,0,
+                                      bin_open_low,bin_low_med,2,
+                                      bin_low_med,100,0),ncol=3, byrow=T))
+
+dw_med <- reclassify(dw_urb, matrix(c(0,0,0,
+                                      0,bin_low_med,0,
+                                      bin_low_med,bin_med_high,3,
+                                      bin_med_high,100,0),ncol=3, byrow=T))
+
+dw_high <- reclassify(dw_urb, matrix(c(0,0,0,
+                                       0,bin_med_high,0,
+                                       bin_med_high,100,4,
+                                       100,101,0),ncol=3, byrow=T))
+dw_pp_urb <- dw_open + dw_low + dw_med + dw_high
+
+# Plot it
+plot(dw_pp_urb, col=c("#E8D1D1","#E29E8C","#ff0000","#B50000"),legend=FALSE,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+legend("topright",legend=c("Open","Low","Medium","High"), fill=c("#E8D1D1","#E29E8C","#ff0000","#B50000"),bg="white")
+title("d)",adj=0.02, line=-1.2, cex.main=1.5)
+title("DW22 sub-classified",adj=0.02, line=-13.9, cex.main=1)
+
+
+
+### e) Watershed areas ########################################################
+# Assign DW bins and reclassify
+dw_all <- reclassify(dw_comp, matrix(c(0,0,NA,
+                                       0,101,1),ncol=3,byrow=T))
+
+dw_all_urb <- dw_comp_urb
+
+dw_open <- reclassify(dw_urb, matrix(c(0,0,NA,
+                                       0,0,NA,
+                                       0,bin_open_low,1,
+                                       bin_open_low,100,NA),ncol=3, byrow=T))
+
+dw_low <- reclassify(dw_urb, matrix(c(0,0,NA,
+                                      0,bin_open_low,NA,
+                                      bin_open_low,bin_low_med,1,
+                                      bin_low_med,100,NA),ncol=3, byrow=T))
+
+dw_med <- reclassify(dw_urb, matrix(c(0,0,NA,
+                                      0,bin_low_med,NA,
+                                      bin_low_med,bin_med_high,1,
+                                      bin_med_high,100,NA),ncol=3, byrow=T))
+
+dw_high <- reclassify(dw_urb, matrix(c(0,0,NA,
+                                       0,bin_med_high,NA,
+                                       bin_med_high,100,1,
+                                       100,101,NA),ncol=3, byrow=T))
+
+# Extract NLCD classifications
+nlcd_all <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,255,1),ncol=3,byrow=T))
+
+nlcd_all_urb <- reclassify(nlcd, matrix(c(0,0,NA,
+                                          0,20,NA,
+                                          20,24,1,
+                                          25,255,NA),ncol=3,byrow=T))
+
+nlcd_open <- reclassify(nlcd, matrix(c(0,0,NA,
+                                       0,20,NA,
+                                       20,21,1,
+                                       21,100,NA),ncol=3, byrow=T))
+
+nlcd_low <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,21,NA,
+                                      21,22,1,
+                                      22,100,NA),ncol=3, byrow=T))
+
+nlcd_med <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,22,NA,
+                                      22,23,1,
+                                      23,100,NA),ncol=3, byrow=T))
+
+nlcd_high <- reclassify(nlcd, matrix(c(0,0,NA,
+                                       0,23,NA,
+                                       23,24,1,
+                                       24,100,NA),ncol=3, byrow=T))
+
+# Calculate area proportions
+nlcd_high_area <- round((cellStats(nlcd_high,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_high_area <- round((cellStats(dw_high,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_med_area <- round((cellStats(nlcd_med,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_med_area <- round((cellStats(dw_med,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_low_area <- round((cellStats(nlcd_low,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_low_area <- round((cellStats(dw_low,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_open_area <- round((cellStats(nlcd_open,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_open_area <- round((cellStats(dw_open,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_all_area <- round((cellStats(nlcd_all_urb,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_all_area <- round((cellStats(dw_all_urb,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+areas_df <- data.frame(NLCD = c(nlcd_high_area, nlcd_med_area, nlcd_low_area, nlcd_open_area, nlcd_all_area),
+                       DW = c(dw_high_area, dw_med_area, dw_low_area, dw_open_area, dw_all_area))
+rownames(areas_df) <- c("High","Med","Low","Open","All urban")
+
+# Make barplot
+par(mar=c(3,3,1,3.5) + 0.1,mgp=c(2,1,0))
+graphics::barplot(t(as.matrix(areas_df)),beside=T,col=NA,border=NA,xlab="Urban type",ylab="% watershed area",
+                  ylim=c(0,35))
+grid()
+box()
+graphics::barplot(t(as.matrix(areas_df)),beside=T,legend=T,add=T,
+                  args.legend = list(bg="white",x="topleft",inset=c(0.1,0)))
+title("e)",adj=0.02, line=-1.2, cex.main=1.5)
+
+
+
+### Create accuracy matrix ####################################################
+
+#### Evaluations: Calculate performance of pixel probs against NLCD urban
+# NLCD#_DW# is the name format
+
+open_open <- cellStats(nlcd_open*dw_open,'sum')
+open_low <- cellStats(nlcd_open*dw_low,'sum')
+open_med <- cellStats(nlcd_open*dw_med,'sum')
+open_high <- cellStats(nlcd_open*dw_high,'sum')
+open_other <- cellStats(nlcd_open, 'sum') - open_open - open_low - open_med - open_high
+
+low_open <- cellStats(nlcd_low*dw_open,'sum')
+low_low <- cellStats(nlcd_low*dw_low,'sum')
+low_med <- cellStats(nlcd_low*dw_med,'sum')
+low_high <- cellStats(nlcd_low*dw_high,'sum')
+low_other <- cellStats(nlcd_low, 'sum') - low_open - low_low - low_med - low_high
+
+med_open <- cellStats(nlcd_med*dw_open,'sum')
+med_low <- cellStats(nlcd_med*dw_low,'sum')
+med_med <- cellStats(nlcd_med*dw_med,'sum')
+med_high <- cellStats(nlcd_med*dw_high,'sum')
+med_other <- cellStats(nlcd_med, 'sum') - med_open - med_low - med_med - med_high
+
+high_open <- cellStats(nlcd_high*dw_open,'sum')
+high_low <- cellStats(nlcd_high*dw_low,'sum')
+high_med <- cellStats(nlcd_high*dw_med,'sum')
+high_high <- cellStats(nlcd_high*dw_high,'sum')
+high_other <- cellStats(nlcd_high, 'sum') - high_open - high_low - high_med - high_high
+
+other_open <- cellStats(dw_open,'sum') - open_open - low_open - med_open - high_open
+other_low <- cellStats(dw_low,'sum') - open_low - low_low - med_low - high_low
+other_med <- cellStats(dw_med,'sum') - open_med - low_med - med_med - high_med
+other_high <- cellStats(dw_high,'sum') - open_high - low_high - med_high - high_high
+
+# Create a table
+col1 <- c(open_open, open_low, open_med, open_high, open_other)
+col2 <- c(low_open, low_low, low_med, low_high, low_other)
+col3 <- c(med_open, med_low, med_med, med_high, med_other)
+col4 <- c(high_open, high_low, high_med, high_high, high_other)
+col5 <- c(other_open, other_low, other_med, other_high, NA) # no other-other
+
+df <- data.frame(NLCD_open=col1, NLCD_low=col2, NLCD_med=col3, NLCD_high=col4, NLCD_other=col5)
+rownames(df) <- c("DW_open", "DW_low", "DW_med", "DW_high","DW_other")
+df
+
+# Create accuracy matrix and calculate overall, producer's, and user's accuracies
+acc_mat <- df[1:4,1:4]
+correct_class <- acc_mat[1,1] + acc_mat[2,2] + acc_mat[3,3] + acc_mat[4,4]
+overall_acc <- correct_class / sum(acc_mat) * 100
+producers_acc <- round(data.frame(open = (acc_mat[1,1] / sum(acc_mat[,1]) * 100), 
+                                  low = (acc_mat[2,2] / sum(acc_mat[,2]) * 100),
+                                  med = (acc_mat[3,3] / sum(acc_mat[,3]) * 100),
+                                  high = (acc_mat[4,4] / sum(acc_mat[,4]) * 100)),2)
+
+users_acc <- round(data.frame(open = (acc_mat[1,1] / sum(acc_mat[1,]) * 100), 
+                              low = (acc_mat[2,2] / sum(acc_mat[2,]) * 100),
+                              med = (acc_mat[3,3] / sum(acc_mat[3,]) * 100),
+                              high = (acc_mat[4,4] / sum(acc_mat[4,]) * 100)),2)
+
+# Make the matrix for display
+acc_mat_dis <- acc_mat
+acc_mat_dis[5,] <- colSums(acc_mat)
+acc_mat_dis[6,] <- producers_acc
+acc_mat_dis[,5] <- c(rowSums(acc_mat[1:4,]),sum(acc_mat),NA)
+acc_mat_dis[,6] <- round(c(t(users_acc),NA,overall_acc),2)
+colnames(acc_mat_dis) <- c("NLCD_open", "NLCD_low", "NLCD_med", "NLCD_high","Total","User's Accuracy")
+rownames(acc_mat_dis) <- c("DW_open","DW_low","DW_med","DW_high","Total", "Producer's Accuracy")
+acc_mat_dis
---
+++ Pixel_class_probabilities/R/Transfer/Transfer evals trees figure.R
@@ -0,0 +1,266 @@
+# BUCK evals figure (trees)
+# Dan Myers, 5/14/2023
+
+
+# Set working directory
+setwd("R:/NPS_NCRN_VitalSigns/Analyses/Projects/AGU23/Scripts/R/Transfer/")
+
+# Load packages
+library(raster)
+library(terra)
+library(unikn) # for text mark
+library(FedData) # NLCD palette
+library(rasterVis) # for levelplot
+library(sf)
+library(tmap)
+library(tmaptools)
+
+# Start plot
+windows(6.5,6.5)
+par(mar=c(1,1,1,1))
+nf <- layout(matrix(c(1,2, # top
+                      3,4, # middle
+                      5,5), # bottom
+                    nrow=3, ncol=2,byrow=TRUE))
+
+
+### a) NLCD ###########################################################
+# Load data
+nlcd_c <- raster("align2_nlcd.tif")
+shed_orig <- st_read("MONO_BUCK_shed.shp")
+shed <- st_transform(shed_orig, crs=st_crs(nlcd_c))
+
+# Crop to watershed
+# nlcd_m <- crop(nlcd,shed)
+# nlcd_c <- mask(nlcd_m,shed)
+
+# Extract forest and reclassify
+nlcd_dec <- reclassify(nlcd_c, matrix(c(0,0,0,
+                                        0,40,0,
+                                        40,41,1,
+                                        41,100,0),ncol=3, byrow=T))
+
+nlcd_eve <- reclassify(nlcd_c, matrix(c(0,0,0,
+                                        0,41,0,
+                                        41,42,2,
+                                        42,100,0),ncol=3, byrow=T))
+
+nlcd_mix <- reclassify(nlcd_c, matrix(c(0,0,0,
+                                        0,42,0,
+                                        42,43,3,
+                                        43,100,0),ncol=3, byrow=T))
+nlcd_for <- nlcd_dec + nlcd_eve + nlcd_mix
+
+# Plot it
+plot(nlcd_for, col=c("#FFFFFF","#85C77E","#38814E","#D4E7B0"),legend=FALSE,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+legend("topright",legend=c("Deciduous","Evergreen","Mixed"), fill=c("#85C77E","#38814E","#D4E7B0"),bg="white")
+title("a)",adj=0.02, line=-1.2, cex.main=1.5)
+title("NLCD19 forest classes",adj=0.02, line=-13.9, cex.main=1)
+
+
+### b) DW original ###########################################################
+# Load data
+dw_comp_uncrop <- raster("align2_comp.tif")
+
+# Crop to watershed
+dw_comp <- mask(dw_comp_uncrop,shed)
+
+
+# Extract tree from DW composite
+dw_comp_for <- reclassify(dw_comp, matrix(c(0,0,NA,
+                                            0,1,1,
+                                            1,10,NA),ncol=3, byrow=T))
+
+# Plot it
+plot(dw_comp_for, col=c("#FFFFFF","#397D49"),legend=FALSE,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+legend("topright",legend="Trees", fill=c("#397D49"),bg="white")
+title("b)",adj=0.02, line=-1.2, cex.main=1.5)
+title("DW22 tree class",adj=0.02, line=-13.9, cex.main=1)
+
+
+### c) DW pixel probs##########################################################
+# Load data
+dw <- raster("align2_trees.tif")
+
+# Mask tree land in DW probs
+dw_tree <- dw * dw_comp_for
+
+# Set colors and plot it
+plot(dw_tree,legend=T,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+title("c)",adj=0.02, line=-1.2, cex.main=1.5)
+title("DW22 tree probability",adj=0.02, line=-13.9, cex.main=1)
+
+
+
+### d) DW sub-classified#######################################################
+##### Set Bins #####
+bin_dec_mix <-61 # highest value in lower bin # 61 is good
+bin_mix_eve <-65                              # 65 is good
+
+# Extract forest types
+dw_dec <- reclassify(dw_tree, matrix(c(0,0,0,
+                                       0,0,0,
+                                       0,bin_dec_mix,1,
+                                       bin_dec_mix,100,0),ncol=3, byrow=T))
+
+dw_eve <- reclassify(dw_tree, matrix(c(0,0,0,
+                                       0,bin_mix_eve,0,
+                                       bin_mix_eve,100,2,
+                                       100,101,0),ncol=3, byrow=T))
+
+dw_mix <- reclassify(dw_tree, matrix(c(0,0,0,
+                                       0,bin_dec_mix,0,
+                                       bin_dec_mix,bin_mix_eve,3,
+                                       bin_mix_eve,100,0),ncol=3, byrow=T))
+dw_pp_for <- dw_dec + dw_eve + dw_mix
+
+
+# Plot it
+plot(dw_pp_for, col=c("#FFFFFF","#85C77E","#38814E","#D4E7B0"),legend=FALSE,axes=FALSE)
+plot(shed$geometry,add=T)
+grid()
+legend("topright",legend=c("Deciduous","Evergreen","Mixed"), fill=c("#85C77E","#38814E","#D4E7B0"),bg="white")
+title("d)",adj=0.02, line=-1.2, cex.main=1.5)
+title("DW22 sub-classified",adj=0.02, line=-13.9, cex.main=1)
+
+
+
+### e) Watershed areas ########################################################
+
+# Assign DW bins and reclassify
+dw_all <- reclassify(dw_comp, matrix(c(0,0,NA,
+                                       0,101,1),ncol=3,byrow=T))
+
+
+dw_dec <- reclassify(dw_tree, matrix(c(0,0,NA,
+                                       0,0,NA,
+                                       0,bin_dec_mix,1,
+                                       bin_dec_mix,100,NA),ncol=3, byrow=T))
+
+dw_mix <- reclassify(dw_tree, matrix(c(0,0,NA,
+                                       0,bin_dec_mix,NA,
+                                       bin_dec_mix,bin_mix_eve,1,
+                                       bin_mix_eve,100,NA),ncol=3, byrow=T))
+
+dw_eve <- reclassify(dw_tree, matrix(c(0,0,NA,
+                                       0,bin_mix_eve,NA,
+                                       bin_mix_eve,100,1,
+                                       100,101,NA),ncol=3, byrow=T))
+dw_all_tree <- dw_comp_for
+
+# Extract NLCD classifications
+nlcd <- nlcd_c
+nlcd_all <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,255,1),ncol=3,byrow=T))
+
+nlcd_all_tree <- reclassify(nlcd, matrix(c(0,0,NA,
+                                           0,40,NA,
+                                           40,43,1,
+                                           43,255,NA),ncol=3,byrow=T))
+
+nlcd_dec <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,40,NA,
+                                      40,41,1,
+                                      41,100,NA),ncol=3, byrow=T))
+
+nlcd_eve <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,41,NA,
+                                      41,42,1,
+                                      42,100,NA),ncol=3, byrow=T))
+
+nlcd_mix <- reclassify(nlcd, matrix(c(0,0,NA,
+                                      0,42,NA,
+                                      42,43,1,
+                                      43,100,NA),ncol=3, byrow=T))
+
+
+# Calculate area proportions
+nlcd_eve_area <- round((cellStats(nlcd_eve,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_eve_area <- round((cellStats(dw_eve,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_mix_area <- round((cellStats(nlcd_mix,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_mix_area <- round((cellStats(dw_mix,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_dec_area <- round((cellStats(nlcd_dec,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_dec_area <- round((cellStats(dw_dec,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+nlcd_all_area <- round((cellStats(nlcd_all_tree,'sum') / cellStats(nlcd_all,'sum') * 100),1)
+dw_all_area <- round((cellStats(dw_all_tree,'sum') / cellStats(dw_all,'sum') * 100),1)
+
+# Put in data frame
+areas_df <- data.frame(NLCD = c(nlcd_eve_area, nlcd_mix_area, nlcd_dec_area, nlcd_all_area),
+                       DW = c(dw_eve_area, dw_mix_area, dw_dec_area, dw_all_area))
+rownames(areas_df) <- c("Evergreen","Mixed","Deciduous","All tree")
+
+# Make barplot
+par(mar=c(3,3,1,3.5) + 0.1,mgp=c(2,1,0))
+graphics::barplot(t(as.matrix(areas_df)),beside=T,col=NA,border=NA,xlab="Forest type",ylab="% watershed area",
+                  ylim=c(0,60))
+grid()
+box()
+graphics::barplot(t(as.matrix(areas_df)),beside=T,legend=T,add=T,
+                  args.legend = list(bg="white",x="topleft",inset=c(0.1,0)))
+title("e)",adj=0.02, line=-1.2, cex.main=1.5)
+
+
+
+### Create accuracy matrix ####################################################
+#### Evaluations: Calculate performance of pixel probs against NLCD
+# NLCD#_DW# is the name format
+
+dec_dec <- cellStats(nlcd_dec*dw_dec,'sum')
+dec_mix <- cellStats(nlcd_dec*dw_mix,'sum')
+dec_eve <- cellStats(nlcd_dec*dw_eve,'sum')
+dec_other <- cellStats(nlcd_dec, 'sum') - dec_dec - dec_mix - dec_eve
+
+mix_dec <- cellStats(nlcd_mix*dw_dec,'sum')
+mix_mix <- cellStats(nlcd_mix*dw_mix,'sum')
+mix_eve <- cellStats(nlcd_mix*dw_eve,'sum')
+mix_other <- cellStats(nlcd_mix, 'sum') - mix_dec - mix_mix - mix_eve
+
+eve_dec <- cellStats(nlcd_eve*dw_dec,'sum')
+eve_mix <- cellStats(nlcd_eve*dw_mix,'sum')
+eve_eve <- cellStats(nlcd_eve*dw_eve,'sum')
+eve_other <- cellStats(nlcd_eve, 'sum') - eve_dec - eve_mix - eve_eve
+
+other_dec <- cellStats(dw_dec,'sum') - dec_dec - mix_dec - eve_dec
+other_mix <- cellStats(dw_mix,'sum') - dec_mix - mix_mix - eve_mix
+other_eve <- cellStats(dw_eve,'sum') - dec_eve - mix_eve - eve_eve
+
+# Create a table
+col1 <- c(dec_dec, dec_mix, dec_eve, dec_other)
+col2 <- c(mix_dec, mix_mix, mix_eve, mix_other)
+col3 <- c(eve_dec, eve_mix, eve_eve, eve_other)
+col4 <- c(other_dec, other_mix, other_eve, NA) # no other-other
+
+df <- data.frame(NLCD_dec=col1, NLCD_mix=col2, NLCD_eve=col3, NLCD_other=col4)
+rownames(df) <- c("DW_dec", "DW_mix", "DW_eve", "DW_other")
+df
+
+# Create accuracy matrix and calculate overall, producer's, and user's accuracies
+acc_mat <- df[1:3,1:3]
+correct_class <- acc_mat[1,1] + acc_mat[2,2] + acc_mat[3,3]
+overall_acc <- correct_class / sum(acc_mat) * 100
+producers_acc <- round(data.frame(dec = (acc_mat[1,1] / sum(acc_mat[,1]) * 100), 
+                                  mix = (acc_mat[2,2] / sum(acc_mat[,2]) * 100),
+                                  eve = (acc_mat[3,3] / sum(acc_mat[,3]) * 100)),2)
+
+users_acc <- round(data.frame(dec = (acc_mat[1,1] / sum(acc_mat[1,]) * 100), 
+                              mix = (acc_mat[2,2] / sum(acc_mat[2,]) * 100),
+                              eve = (acc_mat[3,3] / sum(acc_mat[3,]) * 100)),2)
+
+# Make the matrix for display
+acc_mat_dis <- acc_mat
+acc_mat_dis[4,] <- colSums(acc_mat)
+acc_mat_dis[5,] <- producers_acc
+acc_mat_dis[,4] <- c(rowSums(acc_mat[1:3,]),sum(acc_mat),NA)
+acc_mat_dis[,5] <- round(c(t(users_acc),NA,overall_acc),2)
+colnames(acc_mat_dis) <- c("NLCD_deciduous", "NLCD_mixed", "NLCD_evergreen","Total","User's Accuracy")
+rownames(acc_mat_dis) <- c("DW_deciduous","DW_mixed","DW_evergreen","Total", "Producer's Accuracy")
+acc_mat_dis
