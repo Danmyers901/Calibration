a813b7bf491224bebfd5e2772ba2979c699368f3
Danmyers901 <71978159+Danmyers901@users.noreply.github.com>
2024-01-02 08:39:09 -0500

    Add files via upload
    Run this script to create the multi-pane classifications figure

---
+++ Pixel_class_probabilities/R/main.R
@@ -0,0 +1,20 @@
+# Classifications main script
+# Dan Myers, 1/2/2024
+
+# Run this script to generate the classifications and manuscript figures.
+
+### For the classifications figure
+# Temperate forest
+source("Temperate forest/Temperate forest figure.R")
+
+# Tropical forest
+source("Tropical forest/Tropical forest figure.R")
+
+# Developed
+source("Urban/Urban figure.R")
+
+# Transfer(trees)
+source("Transfer/Transfer evals trees figure.R")
+
+# Transfer(developed)
+source("Transfer/Transfer evals built figure.R")
